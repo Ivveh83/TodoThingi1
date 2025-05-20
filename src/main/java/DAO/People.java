8c2ed7454a50146a627170c84656287136e66345
Ivar Högblom <ivar.hogblom@hotmail.com>
2025-05-20 20:13:00 +0200

    Thirteenth commit. Creating Base<T> generic Interference, adding methods and extending People.java and TodoItems.java with Base.

--- src/main/java/DAO/People.java
+++ src/main/java/DAO/People.java
@@ -4,11 +4,6 @@ import model.Person;
 
 import java.util.Collection;
 
-public interface People {
-    Person create(Person person);
-    Collection<Person> findAll();
-    Person findById(int id);
+public interface People extends Base<Person> {
     Collection<Person> findByName(String name);
-    Person update(Person person);
-    boolean deleteById(int id);
 }
--- src/main/java/DAO/TodoItems.java
+++ src/main/java/DAO/TodoItems.java
@@ -4,14 +4,9 @@ import model.Person;
 import model.Todo;
 import java.util.Collection;
 
-public interface TodoItems {
-    Todo create(Todo todo);
-    Collection<Todo> findAll();
-    Todo findById(int id);
+public interface TodoItems extends Base<Todo> {
     Collection<Todo> findByDoneStatus(boolean done);
     Collection<Todo> findByAssignee(int id);
     Collection<Todo> findByAssignee(Person person);
     Collection<Todo> findByUnassignedTodoItems();
-    Todo update(Todo todo);
-    boolean deleteById(int id);
 }
